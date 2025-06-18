144b99f047d7c09b2945d4297372af573c9b2d63
yuya.hsk <ury1318@gmail.com>
2025-06-18 23:33:49 +0900

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-function Home() {
-  const navigate = useNavigate();
-
-  const handleSelectArea = (areaName) => {
-    const areaMap = {
-      '食堂': 'syokudo',
-      '大浴場': 'daiyokujo',
-      'コミュニティスペース': 'communitySpace'
-    };
-    const path = areaMap[areaName] || areaName; // Fallback to original name if not mapped
-    navigate(`/congestion/${path}`);
-  };
-
+function Home({ onSelect }) {
   return (
     <div className="Home">
       <h1>エリアを選んでください</h1>
-      <button className="button-style" onClick={() => handleSelectArea('食堂')}>食堂</button>
-      <button className="button-style" onClick={() => handleSelectArea('大浴場')}>大浴場</button>
-      <button className="button-style" onClick={() => handleSelectArea('コミュニティスペース')}>コミュニティスペース</button>
+      <button className="button-style" onClick={() => onSelect('食堂')}>食堂</button>
+      <button className="button-style" onClick={() => onSelect('大浴場')}>大浴場</button>
+      <button className="button-style" onClick={() => onSelect('コミュニティスペース')}>コミュニティスペース</button>
     </div>
   );
 }
